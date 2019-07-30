ecc3d305314ae2bb7a99b6fb019b9d3ccad72027
alick97 <alick97@outlook.com>
2019-07-30 12:01:19 +0800

    chapter16: add usmart for debug

--- USER/main.c
+++ USER/main.c
@@ -3,6 +3,7 @@
 #include "usart.h"
 #include "led.h"
 #include "lcd.h"
+#include "usmart.h"
 
 
 /*********************************************************************************
@@ -23,6 +24,29 @@
  *	作者：正点原子 @ALIENTEK
  *	******************************************************************************/
 
+/**
+ * @brief	LED状态设置函数
+ *
+ * @param   sta		状态
+ *
+ * @return  void
+ */
+void led_set(u8 sta)
+{
+	LED_R(sta);
+}
+/**
+ * @brief	函数参数调用测试函数
+ *
+ * @param   ledset		函数指针
+ * @param   sta			灯的状态
+ *
+ * @return  void
+ */
+void test_fun(void(*ledset)(u8),u8 sta)
+{
+	ledset(sta);
+}
 
 int main(void)
 {
@@ -32,71 +56,85 @@ int main(void)
     SystemClock_Config();	            //初始化系统时钟为80M
     delay_init(80);                		//初始化延时函数
     uart_init(115200);
+    usmart_dev.init(80);	//初始化USMART		80M系统时钟	
 	
 	  LED_Init();				//初始化LED
 	  
 	  LCD_Init();				//初始化LCD
-    
-	  while(1)
+
+		POINT_COLOR = RED;
+		Display_ALIENTEK_LOGO(0, 0);
+    LCD_ShowString(30, 95, 200, 16, 16, "Pandora STM32L4 IOT");
+    LCD_ShowString(30, 115, 200, 16, 16, "USMART TEST");
+    LCD_ShowString(30, 135, 200, 16, 16, "ATOM@ALIENTEK");
+    LCD_ShowString(30, 155, 200, 16, 16, "2018/10/27");
+	
+	
+		while(1)
     {
-			  switch(color % 12)
-        {
-            case 0:
-                LCD_Clear(WHITE);
-                break;
-            case 1:
-                LCD_Clear(BLACK);
-                break;
-            case 2:
-                LCD_Clear(BLUE);
-                break;
-            case 3:
-                LCD_Clear(RED);
-                break;
-            case 4:
-                LCD_Clear(MAGENTA);
-                break;
-            case 5:
-                LCD_Clear(GREEN);
-                break;
-            case 6:
-                LCD_Clear(CYAN);
-                break;
-            case 7:
-                LCD_Clear(YELLOW);
-                break;
-            case 8:
-                LCD_Clear(BRRED);
-                break;
-            case 9:
-                LCD_Clear(GRAY);
-                break;
-            case 10:
-                LCD_Clear(LGRAY);
-                break;
-            case 11:
-                LCD_Clear(BROWN);
-                break;
-            default :
-                color = 0;
-                break;
-        }
-
-        Display_ALIENTEK_LOGO(0, 0);
-        POINT_COLOR = RED;
-        BACK_COLOR = WHITE;
-        LCD_ShowString(0, 100, 240, 32, 32, "Pandora STM32L4");
-        LCD_ShowString(0, 140, 240, 24, 24, "TFTLCD TEST 240*240");
-
-        POINT_COLOR = WHITE;
-        BACK_COLOR = BLUE;
-        LCD_ShowString(0, 170, 240, 16, 16, "ATOM@ALIENTEK");
-        LCD_ShowString(0, 200, 240, 12, 12, "2018/10/28");
-				LCD_ShowString(0, 220, 240, 12, 12, "-- alick --");
-        color++;
-        LED_B_TogglePin;
-        delay_ms(1000);
+        LED_B_TogglePin;	//指示运行状态
+        delay_ms(300);
     }
+//	  while(1)
+//    {
+//			  switch(color % 12)
+//        {
+//            case 0:
+//                LCD_Clear(WHITE);
+//                break;
+//            case 1:
+//                LCD_Clear(BLACK);
+//                break;
+//            case 2:
+//                LCD_Clear(BLUE);
+//                break;
+//            case 3:
+//                LCD_Clear(RED);
+//                break;
+//            case 4:
+//                LCD_Clear(MAGENTA);
+//                break;
+//            case 5:
+//                LCD_Clear(GREEN);
+//                break;
+//            case 6:
+//                LCD_Clear(CYAN);
+//                break;
+//            case 7:
+//                LCD_Clear(YELLOW);
+//                break;
+//            case 8:
+//                LCD_Clear(BRRED);
+//                break;
+//            case 9:
+//                LCD_Clear(GRAY);
+//                break;
+//            case 10:
+//                LCD_Clear(LGRAY);
+//                break;
+//            case 11:
+//                LCD_Clear(BROWN);
+//                break;
+//            default :
+//                color = 0;
+//                break;
+//        }
+
+//        Display_ALIENTEK_LOGO(0, 0);
+//        POINT_COLOR = RED;
+//        BACK_COLOR = WHITE;
+//        LCD_ShowString(0, 100, 240, 32, 32, "Pandora STM32L4");
+//        LCD_ShowString(0, 140, 240, 24, 24, "TFTLCD TEST 240*240");
+
+//        POINT_COLOR = WHITE;
+//        BACK_COLOR = BLUE;
+//        LCD_ShowString(0, 170, 240, 16, 16, "ATOM@ALIENTEK");
+//        LCD_ShowString(0, 200, 240, 12, 12, "2018/10/28");
+//				LCD_ShowString(0, 220, 240, 12, 12, "-- alick --");
+//        color++;
+//        LED_B_TogglePin;
+//        delay_ms(1000);
+//    }
 
 }
 
