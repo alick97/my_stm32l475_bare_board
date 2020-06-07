d7289a6f1d3ce9a6a976157d31501d5bea3ea1dc
alick <alick97@outlook.com>
2020-06-08 01:19:59 +0800

    led: Using pin show rgb led.

---
+++ stm32l475-atk-pandora/applications/include/led_sample.h
@@ -0,0 +1,14 @@
+#ifndef __LED_SAMPLE_H__
+#define __LED_SAMPLE_H__
+
+#include <rtthread.h>
+#include <rtdevice.h>
+#include <board.h>
+#define DBG_SECTION_NAME  "main"
+#define DBG_LEVEL         DBG_LOG
+#include <rtdbg.h>
+
+
+void led_sample();
+
+#endif
--- stm32l475-atk-pandora/applications/main.c
+++ stm32l475-atk-pandora/applications/main.c
@@ -8,27 +8,9 @@
  * 2018-11-06     SummerGift   first version
  */
 
-#include <rtthread.h>
-#include <rtdevice.h>
-#include <board.h>
+#include "led_sample.h"
 
-/* defined the LED0 pin: PE7 */
-#define LED0_PIN    GET_PIN(E, 7)
-
-int main(void)
-{
-    int count = 1;
-    /* set LED0 pin mode to output */
-    rt_pin_mode(LED0_PIN, PIN_MODE_OUTPUT);
-
-    while (count++)
-    {
-        rt_pin_write(LED0_PIN, PIN_HIGH);
-        rt_thread_mdelay(500);
-        rt_pin_write(LED0_PIN, PIN_LOW);
-        rt_thread_mdelay(500);
-        rt_kprintf("count: %d\n", count);
-    }
-
+int main(void) {
+    led_sample();
     return RT_EOK;
 }
